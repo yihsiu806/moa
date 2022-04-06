8c7695315c3ff7ddc6d3da44df3e85b9b2e10d11
Yihsiu Chen <yihsiu.chen@icloud.com>
2022-04-06 16:04:52 -0400

    Add user seeder to create three default users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Division;
 use App\Models\Files;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,44 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
+    {
+        $this->users();
+        // $this->files();
+    }
+
+    public function users()
+    {
+        User::insert([
+            'username' => 'admin',
+            'password' => Hash::make('admin'),
+            'role' =>  'admin',
+            'division' => null,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        $firstDivision = Division::select('id')->first();
+        if ($firstDivision) {
+            $firstDivision = $firstDivision->id;
+        }
+        User::insert([
+            'username' => 'division',
+            'password' => Hash::make('division'),
+            'role' =>  'division',
+            'division' => $firstDivision,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        User::insert([
+            'username' => 'vierer',
+            'password' => Hash::make('vierer'),
+            'role' =>  'viewer',
+            'division' => null,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+
+    public function files()
     {
         Files::factory(100)->create();
     }
